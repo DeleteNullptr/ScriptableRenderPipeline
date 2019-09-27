ff461c16d1afaaee3b42e983318fa0ef13a3c659
Anis <anis@unity3d.com>
2019-09-27 16:14:58 +0200

    Review corrections

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
@@ -975,6 +975,8 @@ namespace UnityEngine.Rendering.HighDefinition
                 bool validIndirectDiffuse = ValidIndirectDiffuseState(hdCamera);
                 cmd.SetGlobalInt(HDShaderIDs._RaytracedIndirectDiffuse, validIndirectDiffuse ? 1 : 0);
                 #endif
+
+                cmd.SetGlobalFloat(HDShaderIDs._ContactShadowOpacity, m_ContactShadows.opacity.value);
             }
         }
 
